fbcbeedef88b807048aedeebc8f2810ce3f8e544
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-15 20:57:19 -0500

    added in comments and reverted service-wroker file back to how it was. Still getting error with failed to fetch my service worker and now error with my icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = [
+const staticFilesToPreCache = [
   "/",
   "/index.html",
   "/styles.css",
@@ -7,70 +7,72 @@ const FILES_TO_CACHE = [
   "/db.js"
 ];
 
-  const CACHE_NAME = 'static-cache-v13';
-  const DATA_CACHE_NAME = 'data-cache-v8';
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-  //Install service worker
-  self.addEventListener('install', evt => {
-      evt.waitUntil(
-          caches.open(CACHE_NAME).then(cache =>{
-              console.log('Your files were pre-cached successfully');
-              return cache.addAll(FILES_TO_CACHE);
-          })
-      );
-      self.skipWaiting();
+  // install service worker
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(staticFilesToPreCache);
+      })
+    );
+  
+    self.skipWaiting();
   });
-
-  // Activate Service Worker
-  self.addEventListener('activate', evt => {
-      evt.waitUntil(
-          caches.keys().then(keyList => {
-              return Promise.all(
-                  keyList.map( key => {
-                      if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                          console.log('Removing old cache data', key);
-                          return caches.delete(key);
-                      }
-                  })
-              );
+  
+  // activate
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
           })
-      );
-      self.clients.claim();
+        );
+      })
+    );
+  
+    self.clients.claim();
   });
-
-  // 5. Fetch Files
-  self.addEventListener('fetch',  evt =>{
-      if (evt.request.url.includes('/api/')) {
-          console.log('[Service Worker] Fetch (data)', evt.request.url);
-
-          evt.respondWith(
-              caches.open(DATA_CACHE_NAME).then(cache => {
-                  return fetch(evt.request)
-                      .then(response  => {
-                          if (response.status === 200) {
-                              cache.put(evt.request.url, response.clone());
-                          }
-                          return response;
-                      })
-                      .catch(err => {
-                          return cache.match(evt.request);
-                      });
-              })
-          );
-
-          return;
-      }
-
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    const {url} = evt.request;
+    if (url.includes("/all") || url.includes("/find")) {
       evt.respondWith(
-          caches.open(CACHE_NAME).then( cache => {
-              return cache.match(evt.request).then(response => {
-                  return response || fetch(evt.request)
-              });
-
-          })
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
       );
+    } else {
+      // respond from static cache, request is not for /api/*
+      evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+    }
   });
-
+  
 
 //   Trying to fix ServiceWorker's 'fetch': Object that was not a Response was passed to respondWith() error by replacing it with await rather than .then(), according to stackoverflow.
 
